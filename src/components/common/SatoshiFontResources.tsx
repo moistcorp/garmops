570579d6715090a4b3de76e0ae4263cbce16dfc9
Rahul <irahulsig@gmail.com>
2026-07-31 18:54:02 +0530

    phase 1: flatten liquid-glass tokens to tech-pack system

--- src/components/common/SatoshiFontResources.tsx
+++ src/components/common/SatoshiFontResources.tsx
@@ -6,16 +6,27 @@ const FONT_SHARE_API_ORIGIN = 'https://api.fontshare.com'
 const FONT_SHARE_CDN_ORIGIN = 'https://cdn.fontshare.com'
 const SATOSHI_VARIABLE_STYLESHEET =
   'https://api.fontshare.com/v2/css?f[]=satoshi@1&display=swap'
+const GOOGLE_FONTS_ORIGIN = 'https://fonts.googleapis.com'
+const GOOGLE_FONTS_CDN_ORIGIN = 'https://fonts.gstatic.com'
+const IBM_PLEX_MONO_STYLESHEET =
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;600;700&display=swap'
 
 export default function SatoshiFontResources() {
   preconnect(FONT_SHARE_API_ORIGIN)
   preconnect(FONT_SHARE_CDN_ORIGIN, { crossOrigin: 'anonymous' })
+  preconnect(GOOGLE_FONTS_ORIGIN)
+  preconnect(GOOGLE_FONTS_CDN_ORIGIN, { crossOrigin: 'anonymous' })
 
-  return (
+  return <>
     <link
       href={SATOSHI_VARIABLE_STYLESHEET}
       rel="stylesheet"
       precedence="satoshi"
     />
-  )
+    <link
+      href={IBM_PLEX_MONO_STYLESHEET}
+      rel="stylesheet"
+      precedence="ibm-plex-mono"
+    />
+  </>
 }
